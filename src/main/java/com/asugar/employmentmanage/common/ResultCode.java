1f589eac36fb25872b4e4592f78be28417fdd730
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-09 14:59:10 +0800

    状态码枚举类

---
+++ src/main/java/com/asugar/employmentmanage/common/ResultCode.java
@@ -0,0 +1,25 @@
+package com.asugar.employmentmanage.common;
+/**
+ * 状态码枚举
+ */
+public enum ResultCode {
+
+    SUCCESS(0, "成功"),
+    FAILURE(500,"失败");
+
+    private long code;
+    private String msg;
+
+    ResultCode(long code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
