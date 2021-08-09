ba3045d3079e38e47facc2a0761b8edd1f18af7c
xingly-cn <xiaoheikeji@vip.qq.com>
2021-08-09 15:43:59 +0800

    mybatis配置文件

--- src/main/java/com/asugar/employmentmanage/EmploymentManageApplication.java
+++ src/main/java/com/asugar/employmentmanage/EmploymentManageApplication.java
@@ -1,8 +1,9 @@
 package com.asugar.employmentmanage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.asugar.employmentmanage.mapper")
 @SpringBootApplication
 public class EmploymentManageApplication {
 
--- src/main/java/com/asugar/employmentmanage/controller/TestController.java
+++ src/main/java/com/asugar/employmentmanage/controller/TestController.java
@@ -2,12 +2,20 @@ package com.asugar.employmentmanage.controller;
 
 import com.asugar.employmentmanage.common.BackResult;
 import com.asugar.employmentmanage.common.ResultCode;
+import com.asugar.employmentmanage.entity.User;
+import com.asugar.employmentmanage.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class TestController {
+
+    @Autowired
+    UserMapper userMapper;
 
     @RequestMapping("/hello")
     public BackResult hello(){
